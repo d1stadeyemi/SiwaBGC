76e35733ef752f2bc7a40d5107c29bef0da875db
Muhammad Ajagbe <d1stadeyemi@gmail.com>
2025-02-22 20:49:51 +0200

    upgraded script3

--- Scripts/siwa_script3.sh
+++ Scripts/siwa_script3.sh
@@ -10,29 +10,36 @@ set -u
 # Calculates the abundance of BGCs in each sample.
 # Compares the putative BGCs with MiBIG and BGC atlas databases for novelty check.
 
-# Define input directory
-INPUT_DIR=$1
+mkdir -p antismash_output
 
-# Checks if an argument was provided
-if [[ -z "$INPUT_DIR" ]]
-then
+# Ensure at least one directory is provided
+if [[ $# -lt 1 ]]; then
     echo "Error: No input directory provided."
-    echo "Usage: $(basename "$0") <MAGS_DIR>"
-    echo "Description: MAGS_DIR should be a directory containing MAG files."
+    echo "Usage: $(basename "$0") <MAGS_DIR> [<MAGS_DIR2>...]"
     exit 1
 fi
 
-# Check if input directory is a valid directory.
-if [[ ! -d "$INPUT_DIR" ]]
-then
-    echo "Error: "$INPUT_DIR" is not a valid directory"
-    echo "Usage: $(basename $0) <MAGS_DIR>"
-    exit 1
-fi
+while [[ $# -gt 0 ]]; then
+    # Define input directory
+    INPUT_DIR=$1
+    SAMPLE_NAME=$(basename "$INPUT_DIR" | cut -d"_" -f1) # Extract sample name
 
-# Check if input directory is empty
-if ! find "$INPUT_DIR" -mindepth 1 | read -r _; then
-    echo "Error: '$INPUT_DIR' is empty."
-    exit 1
-fi
+    # Check if input directory is not valid.
+    if [[ ! -d "$INPUT_DIR" ]]
+    then
+        echo "Error: "$INPUT_DIR" is not a valid directory"
+        echo "Usage: $(basename $0) <MAGS_DIR>"
+        exit 1
+    fi
 
+    # Check if input directory is empty
+    if ! find "$INPUT_DIR" -mindepth 1 | read -r _; then
+        echo "Error: '$INPUT_DIR' is empty."
+        exit 1
+    fi
+
+    # 1. Detect BGCs in samples
+    conda activate antismash
+    echo "Running Antismash on ${SAMPLE_NAME}_MAGs..."
+
+done

0cbf92bd16791603c58eef3beb821f5be807e088
Muhammad Ajagbe <d1stadeyemi@gmail.com>
2025-03-10 18:59:33 +0200

    upgraded script3

--- Scripts/siwa_script3.sh
+++ Scripts/siwa_script3.sh
@@ -6,9 +6,9 @@ source ~/miniconda3/etc/profile.d/conda.sh
 # Error on undefined variables
 set -u 
 
-# This script detects BGCs from MAGs and unbinned contigs Fasta files.
-# Calculates the abundance of BGCs in each sample.
-# Compares the putative BGCs with MiBIG and BGC atlas databases for novelty check.
+# This script detects BGCs from MAGs and unbinned contigs Fasta files. 
+# Calculates the abundance of BGCs in each sample. 
+# Compares the putative BGCs with MiBIG and BGC atlas databases for novelty check. 
 
 mkdir -p logs antismash_output BGCs bigmap_output
 

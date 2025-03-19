cdba8b8ff4d4f9f9df593b57b20c0728e685292a
Muhammad Ajagbe <d1stadeyemi@gmail.com>
2025-03-19 19:20:25 +0200

    upgraded script1

--- Scripts/siwa_script1.sh
+++ Scripts/siwa_script1.sh
@@ -10,8 +10,8 @@ set -o pipefail # Detect errors in pipeline
 # This script receives merged paired-end reads. 
 # Conducts quality control on the reads. 
 # Measures sequencing quality. 
-# Taxonomic classification of reads. 
-# Determines microbial diversity among samples. 
+# Taxonomic classification of reads.
+# Determines microbial diversity among samples.
 
 # Check if at least one pair of reads is given 
 if [[ $# -lt 2 || $(($# % 2)) -ne 0 ]]; then
@@ -19,7 +19,7 @@ if [[ $# -lt 2 || $(($# % 2)) -ne 0 ]]; then
     exit 1
 fi
 
-# Create output directories if not created  
+# Create output directories if not created
 mkdir -p logs fastp_output nonpareil_output
 
 # Initialize samples array

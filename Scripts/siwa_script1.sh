d86219fbea37822eb46d8e5e3407dd1b8b9c4347
Muhammad Ajagbe <d1stadeyemi@gmail.com>
2025-04-22 16:30:11 +0200

    upgraded script1

--- Scripts/siwa_script1.sh
+++ Scripts/siwa_script1.sh
@@ -8,8 +8,8 @@ set -u # Error on undefined variables
 set -o pipefail # Detect errors in pipeline 
 
 # This script receives merged paired-end reads.
-# Conducts quality control on the reads. 
-# Measures sequencing quality. 
+# Conducts quality control on the reads.
+# Measures sequencing quality.
 # Taxonomic classification of reads.
 # Determines microbial diversity among samples.
 

122e1a66e1375c8e25b0272dafda33e3bedcda76
Muhammad Ajagbe <d1stadeyemi@gmail.com>
2025-04-25 19:04:56 +0300

    upgraded script1

--- Scripts/siwa_script1.sh
+++ Scripts/siwa_script1.sh
@@ -10,10 +10,10 @@ set -o pipefail # Detect errors in pipeline
 # This script receives merged paired-end reads. 
 # Conducts quality control on the reads. 
 # Measures sequencing quality.
-# Taxonomic classification of reads.
-# Determines microbial diversity among samples.
+# Taxonomic classification of reads. 
+# Determines microbial diversity among samples. 
 
-# Check if at least one pair of reads is given
+# Check if at least one pair of reads is given 
 if [[ $# -lt 2 || $(($# % 2)) -ne 0 ]]; then
     echo "Usage: $0 Sample1_R1 Sample1_R2 [Sample2_R1 Sample2_R2...]"
     exit 1

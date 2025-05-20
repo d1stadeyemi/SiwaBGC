b5ef7736f320d92b4b62567e12e6afbd0fb3c1dd
Muhammad Ajagbe <d1stadeyemi@gmail.com>
2025-05-20 22:32:37 +0300

    upgraded script2

--- Scripts/siwa_script2.sh
+++ Scripts/siwa_script2.sh
@@ -3,10 +3,10 @@
 # Load Conda 
 source ~/miniconda3/etc/profile.d/conda.sh
 
-# Error on undefined variables 
+# Error on undefined variables
 set -u 
 
-# This script assembles metagenomic reads into contigs. 
+# This script assembles metagenomic reads into contigs.
 # Bins the contigs and select MAGs.
 # Classifies the MAGs into taxa.
 # Place MAGs within the GTDB reference genomes for phylogenomy.
